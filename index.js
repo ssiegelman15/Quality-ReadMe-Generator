75c7ce72362bcc8c3aa8d8358529be997f764033
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-04-20 18:57:12 -0600

    started formatting questions for inquirer

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const markdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = ["What is your Github username?", "What is your email address?", "What is your project's name?", 
-"Please write a short description of your project: ", "What kind of license should your project have?", "What command should be run to install dependencies?",
-"What command should be run to run tests?", "What does the used need to know about using the repo?", "What does the user need to know about contributing to the repo?"];
+const questions = [
+  {
+    type: 'input',
+    name: 'github',
+    message: "What is your Github username?",
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?'
+  },
+  {
+    type: 'input',
+    name: 'project',
+    message: "What is your project's name?",
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Please write a short description of your project: ',
+  },
+  {
+    type: 'checkbox',
+    name: 'license',
+    message: "What kind of license should your project have?",
+    choices: ['MIT','APACHE 2.0','GPL 3.0', 'BSD 3', 'None of the above'],
+  },
+
+
+
+  "What command should be run to install dependencies?",
+  "What command should be run to run tests?", "What does the used need to know about using the repo?", "What does the user need to know about contributing to the repo?"];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
