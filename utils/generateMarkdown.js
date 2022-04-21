f2439fe37b7386906108c06fb36c79d2490c638f
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-04-21 10:47:15 -0600

    changed text variable to licenseText

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -50,7 +50,7 @@ function renderLicenseSection(license) {
   switch (license) {
 
     case 'MIT':
-      text = `Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: 
+      licenseText = `Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: 
       
       The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. 
       
@@ -60,7 +60,7 @@ function renderLicenseSection(license) {
       break;
 
     case 'APACHE 2.0':
-      text = `Licensed under the Apache License, Version 2.0 (the "License");
+      licenseText = `Licensed under the Apache License, Version 2.0 (the "License");
       you may not use this file except in compliance with the License.
       You may obtain a copy of the License at
       
@@ -74,7 +74,7 @@ function renderLicenseSection(license) {
       break;
 
     case 'GPL 3.0':
-      text = `This program is free software: you can redistribute it and/or modify
+      licenseText = `This program is free software: you can redistribute it and/or modify
       it under the terms of the GNU General Public License as published by
       the Free Software Foundation, either version 3 of the License, or
       (at your option) any later version.
@@ -89,7 +89,7 @@ function renderLicenseSection(license) {
       break;
     
     case 'BSD 3':
-      text = `Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+      licenseText = `Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 
       1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
       
@@ -103,10 +103,10 @@ function renderLicenseSection(license) {
       break;
     
     default:
-      text = '';
+      licenseText = '';
       break;
   }
-  return text;
+  return licenseText;
 }
 
 // TODO: Create a function to generate markdown for README
