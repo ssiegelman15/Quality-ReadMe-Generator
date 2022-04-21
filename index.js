5f41b9ff94cb9c58ae333088ae1ad72dffb323d0
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-04-20 18:39:01 -0600

    added packages needed and array of questions

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is your Github username?", "What is your email address?", "What is your project's name?", 
+"Please write a short description of your project: ", "What kind of license should your project have?", "What command should be run to install dependencies?",
+"What command should be run to run tests?", "What does the used need to know about using the repo?", "What does the user need to know about contributing to the repo?"];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
